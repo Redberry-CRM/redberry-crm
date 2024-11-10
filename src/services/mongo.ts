4e7d8b636cdbae2a36d1de831f33342ad1aabe40
David Marom <davidmarom@hotmail.com>
2024-11-10 19:58:14 +0200

    mongo

--- src/services/mongo.ts
+++ src/services/mongo.ts
@@ -1,19 +1,24 @@
 // 'use client';
 import { MongoClient, ObjectId } from "mongodb";
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
+// let client: MongoClient;
+// let clientPromise: Promise<MongoClient>;
+
+// export async function connectDatabase() {
+//     if (!client) {
+//         const dbConnectionString = process.env.PUBLIC_DB_CONNECTION;
+//         if (!dbConnectionString) {
+//             throw new Error('Database connection string is not defined');
+//         }
+//         client = new MongoClient(dbConnectionString);
+//         clientPromise = client.connect();
+//     }
+//     return clientPromise;
+// }
 
 export async function connectDatabase() {
-    if (!client) {
-        const dbConnectionString = process.env.PUBLIC_DB_CONNECTION;
-        if (!dbConnectionString) {
-            throw new Error('Database connection string is not defined');
-        }
-        client = new MongoClient(dbConnectionString);
-        clientPromise = client.connect();
-    }
-    return clientPromise;
+    const dbConnection: any = process.env.PUBLIC_DB_CONNECTION;
+    return await MongoClient.connect(dbConnection);
 }
 
 export async function insertDocument(client: any, collection: string, document: object) {
